9525cb8c6ec49196dfe39a4161f5a488c4a3854d
Akash Khandagale <akashakhandagale@gmail.com>
2019-08-18 19:53:33 +0530

    first commit

---
+++ customcalendar/src/main/java/com/akashk/customcalendar/ToasterMessage.kt
@@ -0,0 +1,15 @@
+package com.akashk.customcalendar
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+
+    companion object{
+
+        fun showToast(context: Context,msg:String){
+
+            Toast.makeText(context,msg,Toast.LENGTH_LONG)
+        }
+    }
+}
